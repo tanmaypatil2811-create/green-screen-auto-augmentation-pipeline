c635c5465b3aabe5f3fb5dd9ab2e2766a83eb3e3
tanmaypatil2811-create <tanmaypatil2811@gmail.com>
2025-10-28 16:47:21 +0530

    Add files via upload

---
+++ PrivateRoute.jsx
@@ -0,0 +1,13 @@
+// components/PrivateRoute.jsx
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+const isAuthenticated = () => {
+  return !!localStorage.getItem('token');
+};
+
+const PrivateRoute = ({ children }) => {
+  return isAuthenticated() ? children : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
